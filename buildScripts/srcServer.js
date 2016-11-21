42c57722df6c1a4773adde65d7e5bbb4263da529
mxlockwood <michael@thedatatier.com>
2016-11-20 17:41:52 -0700

    Progress-Linting-is-next

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var path = require('path');
-var open = require('open');
-
+import open from 'open'; //Babel > ES6
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
 var port = 3000;
-var app = express();
+const app = express(); //Babel > ES6
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+}));
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../src/index.html'));
